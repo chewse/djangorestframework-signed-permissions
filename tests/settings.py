f976d7f2eec5c2d795873c1ff59ca143ab798de7
James Pulec <jpulec@gmail.com>
2015-07-19 16:49:47 -0700

    Add some testing infrastructure

---
+++ tests/settings.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.conf.global_settings import *
+import os
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+ROOT_URLCONF = 'tests.urls'
+
+SECRET_KEY = '12345678'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'tests',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'djangorestframework-signed-permissions.db',
+    }
+}
